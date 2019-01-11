db5b9abf1c668a005bd949c76d01b9809f78e261
Jeremy Trifilo <38505188+JeremyDX@users.noreply.github.com>
2019-01-11 16:52:38 -0500

    Update IntegerToCharArrayTesting.cpp

--- C++ Examples/IntegerToCharArrayTesting.cpp
+++ C++ Examples/IntegerToCharArrayTesting.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <DirectXMath.h>
 #include <windows.h>
 #include "GameTime.h"
 
@@ -664,195 +663,174 @@ char* AddDynamicallyToBuffer(int64_t num)
 
 char *Integer64ToCharArray(int64_t num)
 {
-    uint64_t value = num;
-    int neg = num < 0;
-    if (neg)
-        value = -num;
-    int len = 0;
-    if (value >= 1000000000) //10 Digit.
-    {
-        if (value >= 100000000000000) //15 Digit.
-        {
-            if (value >= 10000000000000000) //17 Digits.
-            {       
-                if (value >= 1000000000000000000)
-                    len = 19;
-                else
-                {
-                    if (value >= 100000000000000000)
-                        len = 18;
-                    else
-                        len = 17;
-                }
-            }
-            else
-            {
-                if (value >= 1000000000000000)
-                    len = 16;
-                else
-                {
-                    len = 15;
-                }
-            }
-        }
-        else {
-            if (value >= 100000000000) // 12 Digits.
-            {
-                if (value >= 10000000000000) //14 Digits.
-                    len = 14;
-                else
-                {
-                    if (value >= 1000000000000) //13 Digits.
-                        len = 13;
-                    else
-                        len = 12; //12 Digits.
-                }
-            }
-            else
-            {
-                if (value >= 10000000000)
-                    len = 11; //11 Digits.
-                else
-                {
-                    len = 10; //10 Digits.
-                }
-            }
-        }
-    }
-    else {
-        if (value >= 100000) //6 Digit.
-        {
-            if (value >= 10000000)
-            {
-                if (value >= 100000000)
-                    len = 9;
-                else
-                    len = 8;
-            } 
-            else
-            {
-                if (value >= 1000000)
-                    len = 7;
-                else
-                    len = 6;
-            }
-        }
-        else {
-            if (value >= 1000)
-            {
-                if (value >= 10000)
-                    len = 5;
-                else
-                    len = 4;
-            }
-            else
-            {
-                if (value >= 100)
-                    len = 3;
-                else
-                {
-                    if (value >= 10)
-                        len = 2;
-                    else
-                        len = 1;
-                }
-            }
-        }
-    }
+	uint64_t value = num;
+	int neg = num < 0;
+	if (neg)
+		value = -num;
+	int len = 0;
+	if (value >= 1000000000) //10 Digit.
+	{
+		if (value >= 100000000000000) //15 Digit.
+		{
+			if (value >= 10000000000000000) //17 Digits.
+			{
+				if (value >= 1000000000000000000)
+					len = 19;
+				else
+				{
+					if (value >= 100000000000000000)
+						len = 18;
+					else
+						len = 17;
+				}
+			}
+			else
+			{
+				if (value >= 1000000000000000)
+					len = 16;
+				else
+				{
+					len = 15;
+				}
+			}
+		}
+		else {
+			if (value >= 100000000000) // 12 Digits.
+			{
+				if (value >= 10000000000000) //14 Digits.
+					len = 14;
+				else
+				{
+					if (value >= 1000000000000) //13 Digits.
+						len = 13;
+					else
+						len = 12; //12 Digits.
+				}
+			}
+			else
+			{
+				if (value >= 10000000000)
+					len = 11; //11 Digits.
+				else
+				{
+					len = 10; //10 Digits.
+				}
+			}
+		}
+	}
+	else {
+		if (value >= 100000) //6 Digit.
+		{
+			if (value >= 10000000)
+			{
+				if (value >= 100000000)
+					len = 9;
+				else
+					len = 8;
+			}
+			else
+			{
+				if (value >= 1000000)
+					len = 7;
+				else
+					len = 6;
+			}
+		}
+		else {
+			if (value >= 1000)
+			{
+				if (value >= 10000)
+					len = 5;
+				else
+					len = 4;
+			}
+			else
+			{
+				if (value >= 100)
+					len = 3;
+				else
+				{
+					if (value >= 10)
+						len = 2;
+					else
+						len = 1;
+				}
+			}
+		}
+	}
 
-    static const uint64_t REDUCTION[20] =
-    {
-        1,
-        1,
-        10,
-        100,
-        1000,
-        10000,
-        100000,
-        1000000,
-        10000000, //8
-        100000000,
-        1000000000,
-        10000000000,
-        100000000000,
-        1000000000000,
-        10000000000000,
-        100000000000000,
-        1000000000000000,
-        10000000000000000, //17
-        100000000000000000, //18
-        1000000000000000000, //19
-    };
+	static const uint64_t REDUCTION[20] =
+	{
+		1,
+		1,
+		10,
+		100,
+		1000,
+		10000,
+		100000,
+		1000000,
+		10000000, //8
+		100000000,
+		1000000000,
+		10000000000,
+		100000000000,
+		1000000000000,
+		10000000000000,
+		100000000000000,
+		1000000000000000,
+		10000000000000000, //17
+		100000000000000000, //18
+		1000000000000000000, //19
+	};
 
-    static char buffer[21];
-    //switch to this if you want unique copies. remember to delete though.
-    //char* buffer = new char[len + neg + 1];
+	static char buffer[21];
+	//switch to this if you want unique copies. remember to delete though.
+	//char* buffer = new char[len + neg + 1];
 
-    if (neg)
-        buffer[0] = '-';
+	int offset = -1;
 
-    buffer[len + neg] = '\0';
+	if (neg)
+		buffer[++offset] = '-';
 
-    uint64_t reduction = REDUCTION[len];
+	uint64_t reduction = REDUCTION[len];
 
-    for (int j = -1; ++j < len; )
-    {
-        int c_value = (value / reduction);
-        buffer[j + neg] = 48 + c_value;
-        value -= reduction * c_value;
-        reduction *= 0.1;
-    }
+	for ( ; --len >= 0 ; )
+	{
+		int c_value = (value / reduction);
+		value -= reduction * c_value;
+		buffer[++offset] = 48 + c_value;
+		reduction = REDUCTION[len];
+	}
 
-    return buffer;
+	buffer[++offset] = 0;
+
+	return buffer;
 }
+
 
 int main()
 {
 	int CYCLES = 100000000;
 
-	int value;
-	scanf_s("%d", &value);
+	int64_t value;
+	scanf_s("%lld", &value);
 
 	GameTime::Begin();
 
 	int j = 0;
 	for (int x = 0; x < CYCLES; ++x)
 	{
-		j += AddDynamicallyToBuffer(value)[0];
+		j += Integer64ToCharArray(value)[0];
 	}
 
 	GameTime::Tick();
 
 	std::cout << "Value Used: " << value << '\n';
 	std::cout << "Offset Size: " << j << '\n';
-	std::cout << "String Data: " << AddDynamicallyToBuffer(value) << '\n';
+	std::cout << "String Data: " << Integer64ToCharArray(value) << '\n';
 
-	/*std::cout << "Test: " << AddDynamicallyToBuffer(-1) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(0) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(9) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(19) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(199) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(19999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(199999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(19999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(199999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(19999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(199999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(19999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(199999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1999999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(19999999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(199999999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(1999999999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(-1999999999999999999) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(10099999699999999970) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(LLONG_MAX) << '\n';
-	std::cout << "Test: " << AddDynamicallyToBuffer(LLONG_MIN) << '\n';*/
+	std::cout << "Test: " << Integer64ToCharArray(LLONG_MAX) << '\n';
+	std::cout << "Test: " << Integer64ToCharArray(LLONG_MIN) << '\n';
 
 	GameTime::DisplayOutputMilliseconds(GameTime::TotalElapsedCPUCycles(), CYCLES);
 	system("pause");
